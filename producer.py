c08dc5ee873ab7d91d3e0f10dc3d5ccaeb600eed
Fouad Trad <79650267+fouadtrad@users.noreply.github.com>
2022-07-26 09:43:58 +0300

    Add files via upload

--- producer.py
+++ producer.py
@@ -12,7 +12,7 @@ from json import dumps
 import csv
 import time
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda K:dumps(K).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='172.31.87.125:9092', value_serializer=lambda K:dumps(K).encode('utf-8'))
 
 with open('tweets.csv', 'r') as file:
   reader = csv.reader(file)
